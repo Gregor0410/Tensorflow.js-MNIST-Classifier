5403045d85947e71999e42d1f6efab21587c3736
Gregor Donachie <gregor.donachie@gmail.com>
2019-05-08 22:08:35 +0100

    Can now display random digits to the screen

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 
 let xTrain;
 let yTrain;
-
+let scaleFactor = 10
 function setup(){
-    createCanvas(28,28);
+    createCanvas(28*scaleFactor,28*scaleFactor);
     //Convert data from mnist.js to format for network to learn
     mnist = mnist.get();
     xTrain = mnist.slice(0,8000).map(x => x.input);
     yTrain = mnist.slice(0,8000).map(x => x.output);
-    loadPixels();
+    background(0);
     displayRandomDigit();
 }
 
@@ -18,7 +18,16 @@ function draw(){
 }
 
 function displayRandomDigit(){
-    let digit = tf.tensor1d(random(xTrain));
-    pixels =  digit.reshape([28,28]).array();
+    let digit = tf.tensor(random(xTrain).map(v=>v*255),[1,28,28,1]);
+    digit = tf.image.resizeNearestNeighbor(digit, [28*scaleFactor,28*scaleFactor]);
+    digit = digit.flatten().arraySync();
+    loadPixels()
+    let i = 0; 
+    for (let x = 0; x < 28*scaleFactor; x++) {
+        for (let y = 0; y < 28*scaleFactor; y++) {
+            set(y,x,digit[i]);
+            i++;
+        }
+    }
     updatePixels();
 }
