aa6d9472731f50f4d59aee28f34d4a527a2c1c16
Gregor Donachie <gregor.donachie@gmail.com>
2019-05-08 21:19:06 +0100

    importing mnist dataset

--- sketch.js
+++ sketch.js
@@ -1,10 +1,24 @@
 
+let xTrain;
+let yTrain;
+
 function setup(){
-    createCanvas(400,400);
+    createCanvas(28,28);
+    //Convert data from mnist.js to format for network to learn
+    mnist = mnist.get();
+    xTrain = mnist.slice(0,8000).map(x => x.input);
+    yTrain = mnist.slice(0,8000).map(x => x.output);
+    loadPixels();
+    displayRandomDigit();
 }
 
 function draw(){
-    background(0);
-    rect(50,50,50,50);
+    //background(0);
+    //rect(50,50,50,50);
+}
 
+function displayRandomDigit(){
+    let digit = tf.tensor1d(random(xTrain));
+    pixels =  digit.reshape([28,28]).array();
+    updatePixels();
 }
